32a92e0fceb296f921b8e3bae12ac9b86c6b7146
swatisri1999 <65355669+swatisri1999@users.noreply.github.com>
2020-05-16 15:10:45 +0530

    Update index.php

--- practice/index.php
+++ practice/index.php
@@ -11,8 +11,8 @@
 <a href="login.php">Please Log In</a>
 </p>
 <p>
-Attempt to go to 
-<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+//Attempt to go to 
+//<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
 </div>
 </body>
 
